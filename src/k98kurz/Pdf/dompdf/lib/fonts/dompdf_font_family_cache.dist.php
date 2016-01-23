69fe9f12bf2fc9f1c602d7acce52b72f587e2d31
Jonathan Voss <k98kurz@gmail.com>
2016-01-23 08:47:34 -0500

    added a bunch of new fonts

--- src/k98kurz/Pdf/dompdf/lib/fonts/dompdf_font_family_cache.dist.php
+++ src/k98kurz/Pdf/dompdf/lib/fonts/dompdf_font_family_cache.dist.php
@@ -108,4 +108,123 @@
     'italic' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed-Italic',
     'normal' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed',
   ),
+  'lato' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Lato_regular',
+    'bold' => DOMPDF_FONT_DIR . 'Lato_700',
+    'italic' => DOMPDF_FONT_DIR . 'Lato_italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Lato_700italic',
+  ),
+  'lora' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Lora_regular',
+    'bold' => DOMPDF_FONT_DIR . 'Lora_700',
+    'italic' => DOMPDF_FONT_DIR . 'Lora_italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Lora_700italic',
+  ),
+  'open sans' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Open Sans_regular',
+    'bold' => DOMPDF_FONT_DIR . 'Open Sans_700',
+    'italic' => DOMPDF_FONT_DIR . 'Open Sans_700italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Open Sans_regular',
+  ),
+  'roboto' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Roboto Slab_regular',
+    'bold' => DOMPDF_FONT_DIR . 'Roboto Slab_700',
+    'italic' => DOMPDF_FONT_DIR . 'Roboto Slab_regular',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Roboto Slab_regular',
+  ),
+  'antic' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Antic_regular',
+    'bold' => DOMPDF_FONT_DIR . 'Antic_regular',
+    'italic' => DOMPDF_FONT_DIR . 'Antic_regular',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Antic_regular',
+  ),
+  'antic slab' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Antic Slab_regular',
+    'bold' => DOMPDF_FONT_DIR . 'Antic Slab_regular',
+    'italic' => DOMPDF_FONT_DIR . 'Antic Slab_regular',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Antic Slab_regular',
+  ),
+  'droid serif' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Droid Serif_regular',
+    'bold' => DOMPDF_FONT_DIR . 'Droid Serif_700',
+    'italic' => DOMPDF_FONT_DIR . 'Droid Serif_italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Droid Serif_700italic',
+  ),
+  'geo' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Geo_regular',
+    'bold' => DOMPDF_FONT_DIR . 'Geo_regular',
+    'italic' => DOMPDF_FONT_DIR . 'Geo_italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Geo_italic',
+  ),
+  'italiana' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Italiana_regular',
+    'bold' => DOMPDF_FONT_DIR . 'Italiana_regular',
+    'italic' => DOMPDF_FONT_DIR . 'Italiana_regular',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Italiana_regular',
+  ),
+  'julius sans one' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Julius Sans One_regular',
+    'bold' => DOMPDF_FONT_DIR . 'Julius Sans One_regular',
+    'italic' => DOMPDF_FONT_DIR . 'Julius Sans One_regular',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Julius Sans One_regular',
+  ),
+  'julius' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Julius Sans One_regular',
+    'bold' => DOMPDF_FONT_DIR . 'Julius Sans One_regular',
+    'italic' => DOMPDF_FONT_DIR . 'Julius Sans One_regular',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Julius Sans One_regular',
+  ),
+  'lancelot' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Lancelot_regular',
+    'bold' => DOMPDF_FONT_DIR . 'Lancelot_regular',
+    'italic' => DOMPDF_FONT_DIR . 'Lancelot_regular',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Lancelot_regular',
+  ),
+  'lateef' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Lateef_regular',
+    'bold' => DOMPDF_FONT_DIR . 'Lateef_regular',
+    'italic' => DOMPDF_FONT_DIR . 'Lateef_regular',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Lateef_regular',
+  ),
+  'liberation mono' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'LiberationMono-Regular',
+    'bold' => DOMPDF_FONT_DIR . 'LiberationMono-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'LiberationMono-Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'LiberationMono-BoldItalic',
+  ),
+  'liberation sans' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'LiberationSans-Regular',
+    'bold' => DOMPDF_FONT_DIR . 'LiberationSans-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'LiberationSans-Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'LiberationSans-BoldItalic',
+  ),
+  'liberation serif' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'LiberationSerif-Regular',
+    'bold' => DOMPDF_FONT_DIR . 'LiberationSerif-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'LiberationSerif-Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'LiberationSerif-BoldItalic',
+  ),
+  'liberation sans narrow' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'LiberationSansNarrow-Regular',
+    'bold' => DOMPDF_FONT_DIR . 'LiberationSansNarrow-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'LiberationSansNarrow-Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'LiberationSansNarrow-BoldItalic',
+  ),
 ) ?>
