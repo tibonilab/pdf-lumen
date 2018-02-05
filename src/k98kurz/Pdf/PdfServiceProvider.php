46707c7b6472df96c997a18ec8b668d6107b965a
Alberto Tiboni <info@tibonilab.com>
2018-02-05 11:38:38 +0100

    Update for Lumen >= 5.4.*
    Hi there,
    app->share() method is deprecated in Lumen >= 5.4.*, can you tag a new version with the app->singleton() method to make it compatible with new versions of Lumen ?
    
    thanks!

--- src/k98kurz/Pdf/PdfServiceProvider.php
+++ src/k98kurz/Pdf/PdfServiceProvider.php
@@ -18,7 +18,7 @@ class PdfServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-		$this->app['pdf'] = $this->app->share(function($app)
+		$this->app->singleton('pdf', function($app)
 		{
 			return new Pdf;
 		});
